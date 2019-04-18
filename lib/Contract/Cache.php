59cf72f4d8e4ab3e13da97a4dbf89d1c8a93bfea
mayunyun1 <mayunyun1@100tal.com>
2019-04-18 10:38:27 +0800

    request计数

--- lib/Contract/Cache.php
+++ lib/Contract/Cache.php
@@ -9,5 +9,8 @@ namespace SP\Contract;
 
 interface Cache
 {
-
+    public function get(string $key): string;
+    public function set(string $key, string $val, int $ttlInSec = null);
+    public function incr(string $key, int $ttlInSec = null): int;
+    public function close();
 }
--- public/index.php
+++ public/index.php
@@ -9,10 +9,12 @@ $c->singleton(\SP\Contract\Application::class, \SP\Application::class);
 $c->singleton(\SP\Contract\Request::class, \SP\Request::class);
 $c->singleton(\SP\Contract\Response::class, \SP\Response::class);
 $c->singleton(\SP\Contract\Route::class, \SP\Route::class);
+$c->singleton(\SP\Contract\Cache::class, \SP\RedisCacheBridge::class);
 
 /**
  * process
  */
 $app = app(\SP\Contract\Application::class, [dirname(__DIR__)]);
+$app->pushMiddleware(\App\Middleware\SimpleCacheRequestCountMiddleware::class);
 $app->pushMiddleware(\App\Middleware\CopyRightMiddleware::class);
 $app->run();
