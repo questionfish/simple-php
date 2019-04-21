c0802640ed28764f71e1623985c6b2ac8c0f65b5
mayunyun1 <mayunyun1@100tal.com>
2019-04-21 15:38:14 +0800

    新增路由

--- config/route.php
+++ config/route.php
@@ -1,5 +1,6 @@
 <?php
 
 return [
+    "/" => ["method" => "get","controller" => App\Http\HelloController::class,"action" => "hello"],
     "/test/test" => ["method" => "post","controller" => App\Http\HelloController::class,"action" => "hello"],
 ];
